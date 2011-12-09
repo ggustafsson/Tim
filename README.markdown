3465e861e39e73919537b10868627857fefc8d92
Göran Gustafsson <gustafsson.g@gmail.com>
2011-12-09 23:12:11 +0100

    Big update. All basic functionality done
    ...

--- README.markdown
+++ README.markdown
@@ -20,6 +20,16 @@ not going to waste my time.
 
 When using the default settings Tim depends on:
 
- Mac OS X: **afplay** and **say**.
+ Mac OS X: **afplay**.
 GNU/Linux: **aplay**.
+
+Tips
+----
+If you use Mac OS X you can put this in ~/.timrc:
+
+WORK_CMD=say
+WORK_ARG="start working again"
+
+BREAK_CMD=say
+BREAK_ARG="take a little break"
 
--- tim.sh
+++ tim.sh
@@ -1,36 +1,49 @@
 #!/usr/bin/env zsh
+# vim: ft=zsh
 
-# Göran Gustafsson <gustafsson.g@gmail.com>
+###############################################################################
+# Written by Göran Gustafsson <gustafsson.g@gmail.com>                        #
+#                                                                             #
+# Web: http://ggustafsson.github.com/Timer-Script-Improved                    #
+# Git: https://github.com/ggustafsson/Timer-Script-Improved                   #
+###############################################################################
 
-# Find latest version at: https://github.com/ggustafsson/Timer-Script-Improved
+stty -echo # Disable display of keyboard input.
 
-VERSION=0.1
-FILENAME=$(echo $0 | sed 's/.*\///')
+VERSION=0.2
+FILENAME=$(echo $0 | sed 's/.*\///') # Get filename from full path.
 
-if [ -f ~/.timrc ]; then
 
-fi
+###############################################################################
+# CONFIGURATION SECTION                                                       #
+###############################################################################
+[ -f ~/.timrc ] && source ~/.timrc # Lazy fix. This is just temporary.
 
 if [[ $OSTYPE == darwin* ]]; then
-	COMMAND=afplay
+	COMMAND=afplay # Comes with Mac OS X.
 else
-	COMMAND=aplay
+	COMMAND=aplay  # Comes with alsa-utils.
 fi
 
-[[ -z COUNTDOWN_CMD ]] && COUNTDOWN_CMD=$COMMAND
-[[ -z COUNTDOWN_ARG ]] && COUNTDOWN_ARG=./audio_files/alarm.wav
+# If these variables are not found in ~/.timrc then set them now.
+[ -z $TIMER_CMD ] && TIMER_CMD=$COMMAND
+[ -z $TIMER_ARG ] && TIMER_ARG=./audio_files/alarm.wav
 
-[[ -z WORK_CMD ]] && WORK_CMD=$COMMAND
-[[ -z WORK_ARG ]] && WORK_ARG=./audio_files/work.wav
+[ -z $WORK_CMD ] && WORK_CMD=$COMMAND
+[ -z $WORK_ARG ] && WORK_ARG=./audio_files/work.wav
 
-[[ -z BREAK_CMD ]] && BREAK_CMD=$COMMAND
-[[ -z BREAK_ARG ]] && BREAK_ARG=./audio_files/break.wav
+[ -z $BREAK_CMD ] && BREAK_CMD=$COMMAND
+[ -z $BREAK_ARG ] && BREAK_ARG=./audio_files/break.wav
 
-[[ -z INTERVAL       ]] && INTERVAL=15
-[[ -z POMODORO_WORK  ]] && POMODORO_WORK=25
-[[ -z POMODORO_BREAK ]] && POMODORO_BREAK=5
-[[ -z POMODORO_STOP  ]] && POMODORO_STOP=4
+[ -z $INTERVAL       ] && INTERVAL=15
+[ -z $POMODORO_WORK  ] && POMODORO_WORK=25
+[ -z $POMODORO_BREAK ] && POMODORO_BREAK=5
+[ -z $POMODORO_STOP  ] && POMODORO_STOP=4
 
+
+###############################################################################
+# INFORMATION SECTION                                                         #
+###############################################################################
 function usage {
 	echo "Usage: $FILENAME [OPTION]... [NUMBER]..."
 	echo "Try '--help' for more information."
@@ -60,32 +73,115 @@ function help {
 function version {
 	echo "Tim (Timer Script Improved) $VERSION\n"
 
-	echo "Web site: <http://x>."
-	echo "Git repository: <http://x>.\n"
+	echo "Web: http://ggustafsson.github.com/Timer-Script"
+	echo "Git: https://github.com/ggustafsson/Timer-Script-Improved\n"
 
 	echo "Written by Göran Gustafsson <gustafsson.g@gmail.com>."
 }
 
-function countdown {
-	echo "countdown mode. $1"
+
+###############################################################################
+# TIMER MODE SECTION                                                          #
+###############################################################################
+function timer {
+	(( MINUTES_IN_SECONDS = $1 * 60 ))
+
+	echo -n "Waiting for $1 "
+	if [[ $1 > 1 ]]; then
+		echo "minutes."
+	else
+		echo "minute."
+	fi
+
+	sleep $MINUTES_IN_SECONDS
+	echo -n "\nALARM SET OFF!"
+	$TIMER_CMD $TIMER_ARG
 }
 
+
+###############################################################################
+# INTERVAL MODE SECTION                                                       #
+###############################################################################
 function interval {
-	echo "interval mode. $1"
+	if [[ -z $1 ]]; then
+		MINUTES=$INTERVAL
+	else
+		MINUTES=$1
+	fi
+	(( MINUTES_IN_SECONDS = $MINUTES * 60 ))
+
+	CURRENT_MODE=work
+
+	echo -n "Starting timer. Interval of $MINUTES "
+	if [[ $MINUTES > 1 ]]; then
+		echo "minutes."
+	else
+		echo "minute."
+	fi
+	echo "Start working now."
+
+	while true; do
+		sleep $MINUTES_IN_SECONDS
+
+		if [[ $CURRENT_MODE == work ]]; then
+			echo -n "\nTAKE A LITTLE BREAK!"
+			$BREAK_CMD $BREAK_ARG
+
+			CURRENT_MODE=break
+		else
+			echo -n "\nSTART WORKING AGAIN!"
+			$WORK_CMD $WORK_ARG
+
+			CURRENT_MODE=work
+		fi
+	done
 }
 
+
+###############################################################################
+# POMODORO MODE SECTION                                                       #
+###############################################################################
 function pomodoro {
-	echo "pomodoro mode."
+	(( POMODORO_WORK_IN_SECONDS  = $POMODORO_WORK  * 60 ))
+	(( POMODORO_BREAK_IN_SECONDS = $POMODORO_BREAK * 60 ))
+
+	CURRENT_MODE=work
+	MINUTES_IN_SECONDS=$POMODORO_WORK_IN_SECONDS
+
+	echo "Starting timer. Pomodoro mode."
+	echo "Start working now."
+
+	while true; do
+		sleep $MINUTES_IN_SECONDS
+
+		if [[ $CURRENT_MODE == work ]]; then
+			echo -n "\nTAKE A LITTLE BREAK!"
+			$BREAK_CMD $BREAK_ARG
+
+			CURRENT_MODE=break
+			MINUTES_IN_SECONDS=$POMODORO_BREAK_IN_SECONDS
+		else
+			echo -n "\nSTART WORKING AGAIN!"
+			$WORK_CMD $WORK_ARG
+
+			CURRENT_MODE=work
+			MINUTES_IN_SECONDS=$POMODORO_WORK_IN_SECONDS
+		fi
+	done
 }
 
+
+###############################################################################
+# OPTIONS AND EXECUTION SECTION                                               #
+###############################################################################
 case $# in
 	1)
 		case $1 in
-			[0-9])         countdown $1 ;;
-			-i|--interval) interval     ;;
-			-p|--pomodoro) pomodoro     ;;
-			-h|--help)     help         ;;
-			-v|--version)  version      ;;
+			[0-9])           timer    $1 ;;
+			-i | --interval) interval    ;;
+			-p | --pomodoro) pomodoro    ;;
+			-h | --help)     help        ;;
+			-v | --version)  version     ;;
 			*)
 				usage && return 1
 			;;
@@ -98,6 +194,6 @@ case $# in
 			usage && return 1
 		fi
 	;;
-	*) usage ;;
+	*) usage && return 1 ;;
 esac
 
