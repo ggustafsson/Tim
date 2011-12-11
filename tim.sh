f8334f9d44a36584a10df3c590712e0a78dcba6c
Göran Gustafsson <gustafsson.g@gmail.com>
2011-12-11 15:51:29 +0100

    Added license
    BSD 2-Clause license information.

--- tim.sh
+++ tim.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env zsh
 # vim: ft=zsh
 
+# Copyright (c) 2011, Göran Gustafsson. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 ###############################################################################
-# Written by Göran Gustafsson <gustafsson.g@gmail.com>.                       #
-#                                                                             #
 # Web: http://ggustafsson.github.com/Timer-Script-Improved                    #
 # Git: https://github.com/ggustafsson/Timer-Script-Improved                   #
 ###############################################################################
@@ -61,15 +83,15 @@ Try '--help' for more information."
 function help {
 	echo "Usage: $FILENAME [OPTION]... [NUMBER]...
 
- [NUMBER]                 wait [NUMBER] minutes before starting alarm.
- -i, --interval [NUMBER]  work [NUMBER] minutes and pause [NUMBER] minutes.
-                            defaults to 15 minutes work and pause.
- -p, --pomodoro           work x minutes and pause x minutes. run x times.
-                            defaults to 25 minutes work, 5 minutes break and
-                            stop after 4 times.
-                            <http://en.wikipedia.org/wiki/Pomodoro_Technique>
- -h, --help               display this help and exit.
- -v, --version            output version information and exit.
+  [NUMBER]                 wait [NUMBER] minutes before starting alarm.
+  -i, --interval [NUMBER]  work [NUMBER] minutes and pause [NUMBER] minutes.
+                             defaults to 15 minutes work and pause.
+  -p, --pomodoro           work x minutes and pause x minutes. run x times.
+                             defaults to 25 minutes work, 5 minutes break and
+                             stop after 4 times.
+                             <http://en.wikipedia.org/wiki/Pomodoro_Technique>
+  -h, --help               display this help and exit.
+  -v, --version            output version information and exit.
 
 Examples:
 
